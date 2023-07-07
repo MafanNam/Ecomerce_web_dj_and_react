b92b79ebd35d53422046c527202e400e2d663217
Maxim Manzik <kepkpkep@gmail.com>
2023-07-07 20:19:45 +0300

    Frontend: user registration and login

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,11 +1,21 @@
 import React from "react";
-import {Container} from "react-bootstrap";
-import Nav from "react-bootstrap/Nav";
-import Navbar from "react-bootstrap/Navbar";
+import {useDispatch, useSelector} from "react-redux";
+import {Container, Nav, Navbar, Row, NavDropdown} from "react-bootstrap";
 import {LinkContainer} from "react-router-bootstrap";
+import {logout} from '../actions/userActions'
 
 
 function Header() {
+
+    const userLogin = useSelector(state => state.userLogin)
+    const {userInfo} = userLogin
+
+    const dispatch = useDispatch()
+
+    const logoutHandler = () => {
+        dispatch(logout())
+    }
+
     return (
         <header>
             <Navbar bg="s" variant="dark" expand="lg" collapseOnSelect>
@@ -23,11 +33,23 @@ function Header() {
                                 </Nav.Link>
                             </LinkContainer>
 
-                            <LinkContainer to='/login'>
-                                <Nav.Link>
-                                    <i className="fas fa-user"></i>Login
-                                </Nav.Link>
-                            </LinkContainer>
+                            {userInfo ? (
+                                <NavDropdown title={userInfo.name} id='username'>
+                                    <LinkContainer to='/profile'>
+                                        <NavDropdown.Item>Profile</NavDropdown.Item>
+                                    </LinkContainer>
+
+                                    <NavDropdown.Item onClick={logoutHandler}>Logout</NavDropdown.Item>
+
+                                </NavDropdown>
+                            ) : (
+                                <LinkContainer to='/login'>
+                                    <Nav.Link>
+                                        <i className="fas fa-user"></i>Login
+                                    </Nav.Link>
+                                </LinkContainer>
+                            )}
+
                         </Nav>
                     </Navbar.Collapse>
                 </Container>
