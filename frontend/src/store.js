df665d5ca5fd476e7a77314b28e9f3900a0a83d3
Maxim Manzik <kepkpkep@gmail.com>
2023-07-09 20:24:10 +0300

    Frontend: admin user

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -7,7 +7,8 @@ import {
     userLoginReducer,
     userRegisterReducer,
     userDetailsReducer,
-    userUpdateProfileReducer
+    userUpdateProfileReducer,
+    userListReducer,
 } from './reducers/userReducers'
 import {orderCreateReducer, orderDetailsReducer, orderPayReducer, orderListMyReduces} from "./reducers/orderReducers";
 
@@ -20,6 +21,7 @@ const reducer = combineReducers({
     userRegister: userRegisterReducer,
     userDetails: userDetailsReducer,
     userUpdateProfile: userUpdateProfileReducer,
+    userList: userListReducer,
     orderCreate: orderCreateReducer,
     orderDetails: orderDetailsReducer,
     orderPay: orderPayReducer,
