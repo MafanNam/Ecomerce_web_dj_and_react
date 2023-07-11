c238676439b14de270ec9e4c9d2ea4192f05fd94
Maxim Manzik <kepkpkep@gmail.com>
2023-07-11 18:56:52 +0300

    Backend: create review func, serializer

--- backend/base/urls/product_urls.py
+++ backend/base/urls/product_urls.py
@@ -9,6 +9,8 @@ urlpatterns = [
     path('update/<str:pk>/', views.updateProduct, name='product-update'),
     path('delete/<str:pk>/', views.deleteProduct, name='product-delete'),
 
+    path('<str:pk>/reviews/', views.createProductReview, name='create-review'),
+
     path('<str:pk>/', views.getProduct, name='product'),
 
 ]
